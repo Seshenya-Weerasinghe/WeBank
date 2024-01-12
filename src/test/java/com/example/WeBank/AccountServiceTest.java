c78a2baac82503e5d62c0f5c055bc59fb47483e1
sahan-hs-fulda <urapola-mudalige.wijesinghe@informatik.hs-fulda.de>
2024-01-12 17:04:47 +0100

    Remove unused test classes

--- src/test/java/com/example/WeBank/AccountServiceTest.java
+++ src/test/java/com/example/WeBank/AccountServiceTest.java
@@ -29,32 +29,6 @@ public class AccountServiceTest {
     private AccountService accountService;
 
     @Test
-    public void testCreateAccount() {
-        Branch sampleBranch = new Branch(1, "Sample Branch");
-        Account sampleAccount = new Account(1, "AC123456789", 1000.0, sampleBranch, "savings");
-
-        when(accountRepository.save(sampleAccount)).thenReturn(sampleAccount);
-
-        Account createdAccount = accountService.createAccount(sampleAccount);
-
-        assertEquals(sampleAccount, createdAccount);
-        verify(accountRepository, times(1)).save(sampleAccount);
-    }
-
-    @Test
-    public void testFindByAccountNumber() {
-        Branch sampleBranch = new Branch(1, "Sample Branch");
-        Account sampleAccount = new Account(1, "AC123456789", 1000.0, sampleBranch, "savings");
-
-        when(accountRepository.findByAccountNumber("AC123456789")).thenReturn(sampleAccount);
-
-        Account foundAccount = accountService.findByAccountNumber("AC123456789");
-
-        assertEquals(sampleAccount, foundAccount);
-        verify(accountRepository, times(1)).findByAccountNumber("AC123456789");
-    }
-
-    @Test
     public void testCalculateAccountCost() {
         Branch sampleBranch = new Branch(1, "Sample Branch");
         Account sampleAccount = new Account(1, "AC123456789", 1000.0, sampleBranch, "Savings");
